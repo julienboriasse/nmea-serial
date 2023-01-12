2eebd9e258659a2c4eced33c91b8282f239c00a0
Julien Boriasse <julien@boriasse.com>
2023-01-12 12:36:27 -0500

    Add escape key

--- nmea-serial.py
+++ nmea-serial.py
@@ -2,6 +2,13 @@ from datetime import datetime, timedelta, timezone
 from nmeasim.models import GpsReceiver
 import time
 import serial
+import keyboard
+
+# Listen to keys and quit script if I press escape key
+def on_press(key):
+    if key == keyboard.Key.esc:
+        return False
+        
 
 # Serial device configuration
 COM_PORT = 'COM20'  # COM port of the serial device
@@ -19,11 +26,20 @@ gps = GpsReceiver(
     output=('RMC',))
 
 # Send the GPS data to the serial port
-while True:
-    # Wait for 1 second
-    time.sleep(1)
+starting_time = time.time()
 
-    gps.date_time += timedelta(seconds=1)
-    gps_output = str(gps.get_output()).encode('utf-8')
-    print(gps_output)
-    ser.write(gps_output)
+while True:
+    if time.time() - starting_time > 1:
+        # Update the date_time variable
+        starting_time = time.time()
+        gps.date_time += timedelta(seconds=1)
+
+        # Get the GPS data
+        gps_output = str(gps.get_output()).encode('utf-8')
+        print(gps_output)
+        ser.write(gps_output)
+
+    # Check if I pressed escape key - q
+    if keyboard.is_pressed("q"):
+        # Key was pressed
+        break
